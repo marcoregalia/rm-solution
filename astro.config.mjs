fc4cfeca966919e70d4325aa985f56ab1f5e7980
Marco Regalia <marco@regaliaweb.com>
2026-05-17 10:49:24 +0200

    Disabilita sitemap temporaneamente per primo deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://www.rm-solution.it',
-  integrations: [sitemap()],
   build: {
     inlineStylesheets: 'auto',
   },
